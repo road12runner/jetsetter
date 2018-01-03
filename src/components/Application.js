13c4b5b4089a5f633b29965dd6a9567131c9776f
dmitry <dmitry.panferov@gmail.com>
2018-01-03 16:58:09 +0100

    Initial commit

---
+++ src/components/Application.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import uniqueId from 'lodash/uniqueId';
+import CountDown from './CountDown';
+import NewItem from './NewItem';
+import Items from './Items';
+
+import './Application.css';
+import Item from './Item';
+
+const defaultState = [
+  { value: 'Pants', id: uniqueId(), packed: false },
+  { value: 'Jacket', id: uniqueId(), packed: false },
+  { value: 'iPhone Charger', id: uniqueId(), packed: false },
+  { value: 'MacBook', id: uniqueId(), packed: false },
+  { value: 'Sleeping Pills', id: uniqueId(), packed: true },
+  { value: 'Underwear', id: uniqueId(), packed: false },
+  { value: 'Hat', id: uniqueId(), packed: false },
+  { value: 'T-Shirts', id: uniqueId(), packed: false },
+  { value: 'Belt', id: uniqueId(), packed: false },
+  { value: 'Passport', id: uniqueId(), packed: true },
+  { value: 'Sandwich', id: uniqueId(), packed: true },
+];
+
+class Application extends Component {
+  state = {
+    items: defaultState
+    // Set the initial state,
+  };
+
+  // How are we going to manipualte the state?
+  // Ideally, users are going to want to add, remove,
+  // and check off items, right?
+
+
+
+  render() {
+    // Get the items from state
+
+    const unpackedItems = this.state.items.filter(item =>  item.packed === false);
+	  const packedItems = this.state.items.filter(item =>  item.packed === true);
+    console.log(unpackedItems);
+
+
+    return (
+      <div className="Application">
+        <NewItem />
+        <CountDown />
+        <Items title="Unpacked Items" items={unpackedItems} />
+        <Items title="Packed Items" items={packedItems} />
+        <button className="button full-width">Mark All As Unpacked</button>
+      </div>
+    );
+  }
+}
+
+export default Application;
